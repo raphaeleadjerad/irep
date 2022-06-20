5e42ab6f5e52823bc9e443835dfa4e127442f6b5
linogaliana <linogaliana@yahoo.fr>
2022-06-20 16:27:06 +0000

    Fonction de pipeline

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -4,7 +4,7 @@ import pandas as pd
 from collections import ChainMap
 from elasticsearch import Elasticsearch
 import geopandas as gpd
-
+import rapidfuzz
 
 # Create filesystem object
 S3_ENDPOINT_URL = "https://" + os.environ["AWS_S3_ENDPOINT"]
@@ -39,26 +39,6 @@ def transform_wgs84(df, epsg):
     etab = pd.DataFrame(etab)
     return etab
 
-
-# ELASTIC
-
-HOST = 'elasticsearch-master.projet-ssplab'
-
-
-def elastic():
-    """Connection avec Elastic sur le data lab"""
-    es = Elasticsearch([{'host': HOST, 'port': 9200, 'scheme': 'http'}], http_compress=True, request_timeout=200)
-    return es
-
-es = elastic()
-
-def get_product_echo(echo):
-    if echo:
-        return echo[0]['_source']
-    else:
-        return None
-
-
 def clean_data_etab(df):
     etab = df
     etab_not_null = etab.dropna(subset = ['code_epsg'])
@@ -80,3 +60,51 @@ def clean_data_etab(df):
     etab = etab.rename({'numero_siret': "numero_siret_true"}, axis = 1)
     etab["code_apet"] = etab["code_ape"].str[:4]
     return etab
+
+
+# ELASTIC
+
+HOST = 'elasticsearch-master.projet-ssplab'
+
+
+def elastic():
+    """Connection avec Elastic sur le data lab"""
+    es = Elasticsearch([{'host': HOST, 'port': 9200, 'scheme': 'http'}], http_compress=True, request_timeout=200)
+    return es
+
+es = elastic()
+
+def get_product_echo(echo):
+    if echo:
+        return echo[0]['_source']
+    else:
+        return None
+
+def pipeline_request(df, request_template, cols):
+
+  multiple_requetes = df.loc[:,cols].apply(
+      lambda s: '{ "index" : "sirus_2020" }\n' + request_template.format_map(s).replace("\n",""), axis = 1
+  )
+  multiple_requetes = "\n".join(multiple_requetes)
+
+  res = es.msearch(body = multiple_requetes, max_concurrent_searches = 500)
+
+  temp = pd.json_normalize(res,record_path=["responses"])
+
+  out_elastic = temp.apply(
+          lambda l: get_product_echo(l['hits.hits']),
+          axis=1, result_type="expand")
+
+  df_out = pd.concat(
+      [df, out_elastic], axis = 1
+      )
+
+  df_out['match_ok'] = (df['numero_siret_true'].astype(str) == df_out['siret'].astype(str))
+  df_out["textual_distance"] = pd.concat(
+          [df_out.apply(lambda x: rapidfuzz.fuzz.partial_ratio(x["denom"], x[y]), axis=1) for y in ["nom_etablissement"]],
+          axis=1
+      )
+
+  return df_out
+
+
